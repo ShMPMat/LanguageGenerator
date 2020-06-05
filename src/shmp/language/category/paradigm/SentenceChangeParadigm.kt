24b9d927ac9170ccdadb466c97da6750f6b5112c
ShMP <matveyshnytkin@gmail.com>
2020-06-05 21:20:15 +0300

    Moved sentence Clause construction to a separate class.

--- src/shmp/language/category/paradigm/SentenceChangeParadigm.kt
+++ src/shmp/language/category/paradigm/SentenceChangeParadigm.kt
@@ -6,7 +6,6 @@ import shmp.language.category.Category
 import shmp.language.category.CategorySource
 import shmp.language.syntax.Clause
 import shmp.language.syntax.Sentence
-import shmp.language.syntax.SentenceNode
 import shmp.language.syntax.SyntaxRelation
 
 class SentenceChangeParadigm(
@@ -14,41 +13,15 @@ class SentenceChangeParadigm(
     val wordChangeParadigm: WordChangeParadigm,
     val referenceHandler: ReferenceHandler
 ) {
-    private val processedNodes = mutableListOf<SentenceNode>()//TODO to a new class
-
     fun apply(sentence: Sentence): Clause {
-        val clausesInParadigm = applyNode(sentence.node)
+        val clausesInParadigm = SentenceClauseConstructor(this).applyNode(sentence.node)
         val resultWords = clausesInParadigm
             .sortedBy { (r) ->
                 val i = sovOrder.referenceOrder.indexOf(r)
                 if (i == -1) throw ChangeException("No Relation $r in a relation order ${sovOrder.referenceOrder}")
                 i
             }.flatMap { it.second.words }
-        processedNodes.clear()
         return Clause(resultWords)
-    }
-
-    //TODO the first is always a Verb!
-    fun applyNode(sentenceNode: SentenceNode): List<NonJoinedClause> =
-        applyNodeInternal(sentenceNode, SyntaxRelation.Verb)
-
-    private fun applyNodeInternal(sentenceNode: SentenceNode, relation: SyntaxRelation): List<NonJoinedClause> {
-        val speechPart = sentenceNode.word.semanticsCore.speechPart
-        val references = wordChangeParadigm.getSpeechPartParadigm(speechPart).categories
-            .map {
-                val reference = referenceHandler(speechPart, it)
-                    ?: throw ChangeException("$speechPart doesn't have a category ${it.outType}")
-                it to reference
-            }
-        val categoryValues = sentenceNode.extractValues(references)
-        val currentClause = relation to wordChangeParadigm.apply(sentenceNode.word, categoryValues)
-        processedNodes.add(sentenceNode)
-        val childrenClauses = sentenceNode.relation
-            .filter { it.value !in processedNodes }
-            .flatMap { (r, n) -> applyNodeInternal(n, r) }
-            .toMutableList()
-        childrenClauses.add(currentClause)
-        return childrenClauses
     }
 
     override fun toString() = """
@@ -60,5 +33,3 @@ class SentenceChangeParadigm(
 
 
 typealias ReferenceHandler = (SpeechPart, Category) -> CategorySource?
-
-typealias NonJoinedClause = Pair<SyntaxRelation, Clause>
---
+++ src/shmp/language/category/paradigm/SentenceClauseConstructor.kt
@@ -0,0 +1,34 @@
+package shmp.language.category.paradigm
+
+import shmp.language.syntax.Clause
+import shmp.language.syntax.SentenceNode
+import shmp.language.syntax.SyntaxRelation
+
+internal class SentenceClauseConstructor(val paradigm: SentenceChangeParadigm) {
+    private val processedNodes = mutableListOf<SentenceNode>()
+
+    //TODO the first is always a Verb!
+    fun applyNode(sentenceNode: SentenceNode): List<NonJoinedClause> =
+        applyNodeInternal(sentenceNode, SyntaxRelation.Verb)
+
+    private fun applyNodeInternal(sentenceNode: SentenceNode, relation: SyntaxRelation): List<NonJoinedClause> {
+        val speechPart = sentenceNode.word.semanticsCore.speechPart
+        val references = paradigm.wordChangeParadigm.getSpeechPartParadigm(speechPart).categories
+            .map {
+                val reference = paradigm.referenceHandler(speechPart, it)
+                    ?: throw ChangeException("$speechPart doesn't have a category ${it.outType}")
+                it to reference
+            }
+        val categoryValues = sentenceNode.extractValues(references)
+        val currentClause = relation to paradigm.wordChangeParadigm.apply(sentenceNode.word, categoryValues)
+        processedNodes.add(sentenceNode)
+        val childrenClauses = sentenceNode.relation
+            .filter { it.value !in processedNodes }
+            .flatMap { (r, n) -> applyNodeInternal(n, r) }
+            .toMutableList()
+        childrenClauses.add(currentClause)
+        return childrenClauses
+    }
+}
+
+typealias NonJoinedClause = Pair<SyntaxRelation, Clause>
