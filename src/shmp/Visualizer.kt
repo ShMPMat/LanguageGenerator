7de5b3ef7decd42a9e6deb144f4a84fbe2239374
ShMP <matveyshnytkin@gmail.com>
2020-06-20 06:59:07 +0300

    Visualizer is now a class.

--- src/shmp/Visualizer.kt
+++ src/shmp/Visualizer.kt
@@ -14,65 +14,77 @@ import shmp.language.syntax.SentenceType
 import shmp.language.syntax.SyntaxRelation
 import kotlin.random.Random
 
-fun visualize(language: Language) {
-    fun Word.toNode(presetCategories: List<CategoryValue> = listOf()): SentenceNode {
-        val classNames = presetCategories
-            .map { it.parentClassName }
 
-        return SentenceNode(
-            this,
-            language.sentenceChangeParadigm.wordChangeParadigm
-                .getDefaultState(this)
-                .filter { it.source == CategorySource.SelfStated }
-                .map { it.categoryValue }
-                .filter { it.parentClassName !in classNames }
-                    + presetCategories
-        )
+data class Visualizer(val language: Language) {
+    fun visualize() {
+        printTestSentences()
+        print("\n\n")
+        print(language)
+        println()
+        printAdditionalLexisInfo()
     }
 
-    val subjAdj = language.words.first { it.semanticsCore.word == "new" }.toNode()
-    val subj = language.words.first { it.semanticsCore.word == "mother" }.toNode(listOf(NumbersValue.Plural))
-    val verb = language.words.first { it.semanticsCore.word == "have" }.toNode()
-    val objAdj = language.words.first { it.semanticsCore.word == "new" }.toNode()
-    val obj = language.words.first { it.semanticsCore.word == "time" }.toNode()
+    fun printTestSentences() {
+        fun Word.toNode(presetCategories: List<CategoryValue> = listOf()): SentenceNode {
+            val classNames = presetCategories
+                .map { it.parentClassName }
 
-    subjAdj.setRelation(SyntaxRelation.Subject, subj)
-    subj.setRelation(SyntaxRelation.Verb, verb)
-    subj.setRelation(SyntaxRelation.Definition, subjAdj)
-    verb.setRelation(SyntaxRelation.Subject, subj)
-    verb.setRelation(SyntaxRelation.Object, obj)
-    objAdj.setRelation(SyntaxRelation.Subject, obj)
-    obj.setRelation(SyntaxRelation.Verb, verb)
-    obj.setRelation(SyntaxRelation.Definition, objAdj)
+            return SentenceNode(
+                this,
+                language.sentenceChangeParadigm.wordChangeParadigm
+                    .getDefaultState(this)
+                    .filter { it.source == CategorySource.SelfStated }
+                    .map { it.categoryValue }
+                    .filter { it.parentClassName !in classNames }
+                        + presetCategories
+            )
+        }
 
-    val testSentenceMain = Sentence(verb, SentenceType.MainClause)
-    val testSentenceQuestion = Sentence(verb, SentenceType.Question)
-    println("Main:")
-    println(getClauseAndInfoPrinted(language.sentenceChangeParadigm.apply(testSentenceMain, Random(10))))
-    println("General question:")
-    println(getClauseAndInfoPrinted(language.sentenceChangeParadigm.apply(testSentenceQuestion, Random(10))))
-    print("\n\n")
+        val subjAdj = language.words.first { it.semanticsCore.word == "new" }.toNode()
+        val subj = language.words.first { it.semanticsCore.word == "mother" }.toNode(listOf(NumbersValue.Plural))
+        val verb = language.words.first { it.semanticsCore.word == "have" }.toNode()
+        val objAdj = language.words.first { it.semanticsCore.word == "new" }.toNode()
+        val obj = language.words.first { it.semanticsCore.word == "time" }.toNode()
 
-    print(language)
-    println()
+        subjAdj.setRelation(SyntaxRelation.Subject, subj)
+        subj.setRelation(SyntaxRelation.Verb, verb)
+        subj.setRelation(SyntaxRelation.Definition, subjAdj)
+        verb.setRelation(SyntaxRelation.Subject, subj)
+        verb.setRelation(SyntaxRelation.Object, obj)
+        objAdj.setRelation(SyntaxRelation.Subject, obj)
+        obj.setRelation(SyntaxRelation.Verb, verb)
+        obj.setRelation(SyntaxRelation.Definition, objAdj)
 
-    val synonyms = language.words
-        .groupBy { it.semanticsCore.word }
-        .map { it.value }
-        .filter { it.size > 1 }
+        val testSentenceMain = Sentence(verb, SentenceType.MainClause)
+        val testSentenceQuestion = Sentence(verb, SentenceType.Question)
+        println("Main:")
+        println(getClauseAndInfoPrinted(language.sentenceChangeParadigm.apply(testSentenceMain, Random(10))))
+        println("General question:")
+        println(getClauseAndInfoPrinted(language.sentenceChangeParadigm.apply(testSentenceQuestion, Random(10))))
+    }
 
-    print("""
+    fun printAdditionalLexisInfo() {
+        val synonyms = language.words
+            .groupBy { it.semanticsCore.word }
+            .map { it.value }
+            .filter { it.size > 1 }
+
+        print(
+            """
         |Synonyms:
         |${synonyms.joinToString("\n") { "$it - ${it[0].semanticsCore.word}" }}
         |
         |Lexis size - ${language.words.size} words
         |
-    """.trimMargin())
+    """.trimMargin()
+        )
+    }
 }
 
 fun main() {
     val generator = LanguageGenerator("SupplementFiles", 203)
     val wordAmount = WordBase("SupplementFiles").baseWords.size
 
-    visualize(generator.generateLanguage(wordAmount))
+    Visualizer(generator.generateLanguage(wordAmount))
+        .visualize()
 }
