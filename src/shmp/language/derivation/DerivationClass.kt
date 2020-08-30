f5e3880ff0f390be5ae7583975ba497676312513
ShMP <matveyshnytkin@gmail.com>
2020-08-30 13:46:53 +0300

    A new "Place from Verb" DerivationType and a bunch of derivations in the Word list.

--- src/shmp/language/derivation/DerivationClass.kt
+++ src/shmp/language/derivation/DerivationClass.kt
@@ -1,26 +1,33 @@
 package shmp.language.derivation
 
 import shmp.language.SpeechPart
+import shmp.language.SpeechPart.*
 import shmp.language.derivation.DerivationType.*
 import shmp.random.SampleSpaceObject
 
 
 enum class DerivationClass(val possibilities: List<Box>, val fromSpeechPart: SpeechPart, val toSpeechPart: SpeechPart) {
-    Diminutive(listOf(Box(Smallness, 1.0), Box(Young, 1.0)), SpeechPart.Noun, SpeechPart.Noun),
-    Augmentative(listOf(Box(Big, 3.0), Box(Old, 1.0)), SpeechPart.Noun, SpeechPart.Noun),
-    Place(listOf(Box(NNPlace, 1.0)), SpeechPart.Noun, SpeechPart.Noun),
-    Person(listOf(Box(NNPerson, 1.0)), SpeechPart.Noun, SpeechPart.Noun),
+    Diminutive(listOf(Box(Smallness, 1.0), Box(Young, 1.0)), Noun, Noun),
+    Augmentative(listOf(Box(Big, 3.0), Box(Old, 1.0)), Noun, Noun),
+
+    PlaceFromNoun(listOf(Box(NNPlace, 1.0)), Noun, Noun),
+    PersonFromNoun(listOf(Box(NNPerson, 1.0)), Noun, Noun),
+    PlaceFromVerb(listOf(Box(VNPlace, 1.0)), Verb, Noun),
+//    PersonFromVerb(listOf(Box(VNPerson, 1.0)), Verb, Noun)
 }
 
 enum class DerivationType(val fromSpeechPart: SpeechPart, val toSpeechPart: SpeechPart) {
-    Smallness(SpeechPart.Noun, SpeechPart.Noun),
-    Young(SpeechPart.Noun, SpeechPart.Noun),
+    Smallness(Noun, Noun),
+    Young(Noun, Noun),
 
-    Big(SpeechPart.Noun, SpeechPart.Noun),
-    Old(SpeechPart.Noun, SpeechPart.Noun),
+    Big(Noun, Noun),
+    Old(Noun, Noun),
 
-    NNPlace(SpeechPart.Noun, SpeechPart.Noun),
-    NNPerson(SpeechPart.Noun, SpeechPart.Noun)
+    NNPlace(Noun, Noun),
+    NNPerson(Noun, Noun),
+
+    VNPlace(Verb, Noun),
+//    VNPerson(Verb, Noun)
 }
 
 data class Box(val type: DerivationType?, override val probability: Double): SampleSpaceObject
