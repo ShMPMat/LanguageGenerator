abc1b42e916337f3b620de2a65628b12824bfaf3
ShMP <matveyshnytkin@gmail.com>
2021-03-19 18:39:51 +0300

    Fixed copula probabilities.

--- src/shmp/language/syntax/features/Copula.kt
+++ src/shmp/language/syntax/features/Copula.kt
@@ -7,7 +7,5 @@ data class CopulaPresence(val copulaType: ParametrizedSyntaxFeatures<CopulaType>
 enum class CopulaType(override val probability: Double): SyntaxFeature {
     Verb(1.0),
     Particle(0.25),
-//    Particle(0.0),
-//    None(0.1)
-    None(0.0)
+    None(0.1)
 }
