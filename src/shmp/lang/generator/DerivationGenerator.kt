0cf29abb269ecd8ca04702913f042282bf90ab14
ShMP <matveyshnytkin@gmail.com>
2021-03-29 05:37:51 +0300

    A new derivation, broke 200 words limit.

--- src/shmp/lang/generator/DerivationGenerator.kt
+++ src/shmp/lang/generator/DerivationGenerator.kt
@@ -14,6 +14,7 @@ import shmp.lang.language.phonology.PhonemeSequence
 import shmp.lang.language.phonology.RestrictionsParadigm
 import shmp.random.randomElement
 import shmp.random.randomSublist
+import shmp.random.singleton.chanceOf
 import shmp.random.testProbability
 import kotlin.random.Random
 
@@ -49,7 +50,7 @@ class DerivationGenerator(
     }
 
     private fun generateDerivations(changeGenerator: ChangeGenerator, categoryPool: CategoryPool) =
-        randomSublist(DerivationClass.values().toList(), random, 0, DerivationClass.values().size + 1)
+        randomSublist(DerivationClass.values().toList(), random, 2, DerivationClass.values().size + 1)
             .map { derivationClass ->
                 val affix = if (random.nextBoolean()) {
                     Prefix(
@@ -104,8 +105,11 @@ class DerivationGenerator(
             compounds.add(compound)
         }
 
-        if (testProbability(0.5, random))
+        0.5.chanceOf {
+            val changer = PassingCategoryChanger(random.nextInt(2))
+            val prosodyRule = generateCompoundProsodyRule()
             compounds.add(Compound(SpeechPart.Noun, PhonemeSequence(), changer, prosodyRule))
+        }
 
         return compounds
     }
--- src/shmp/lang/language/derivation/DerivationClass.kt
+++ src/shmp/lang/language/derivation/DerivationClass.kt
@@ -14,7 +14,8 @@ enum class DerivationClass(val possibilities: List<Box>, val fromSpeechPart: Spe
     PlaceFromNoun(listOf(Box(NNPlace, 1.0)), Noun, Noun),
     PersonFromNoun(listOf(Box(NNPerson, 1.0)), Noun, Noun),
     PlaceFromVerb(listOf(Box(VNPlace, 1.0)), Verb, Noun),
-    PersonFromVerb(listOf(Box(VNPerson, 1.0)), Verb, Noun)
+    PersonFromVerb(listOf(Box(VNPerson, 1.0)), Verb, Noun),
+    AbstractNounFromVerb(listOf(Box(VNAbstract, 1.0)), Verb, Noun)
 }
 
 enum class DerivationType(val fromSpeechPart: SpeechPart, val toSpeechPart: SpeechPart) {
@@ -28,8 +29,9 @@ enum class DerivationType(val fromSpeechPart: SpeechPart, val toSpeechPart: Spee
     NNPerson(Noun, Noun),
 
     VNPlace(Verb, Noun),
-    VNPerson(Verb, Noun)
-}//TODO VNAbstract NNAbstract
+    VNPerson(Verb, Noun),
+    VNAbstract(Verb, Noun)
+}//TODO NNAbstract
 
 data class Box(val type: DerivationType?, override val probability: Double): UnwrappableSSO<DerivationType?>(type)
 
