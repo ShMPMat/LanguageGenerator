a246ff399aed516490ce83edc0753b6cefa41e70
smp <matveyshnytkin@gmail.com>
2024-08-31 13:34:31 +0400

    Set Phoneme defaults

--- src/io/tashtabash/lang/language/phonology/Phoneme.kt
+++ src/io/tashtabash/lang/language/phonology/Phoneme.kt
@@ -8,7 +8,7 @@ data class Phoneme(
     val type: PhonemeType,
     val articulationPlace: ArticulationPlace,
     val articulationManner: ArticulationManner,
-    val modifiers: Set<PhonemeModifier>
+    val modifiers: Set<PhonemeModifier> = setOf()
 ) {
     override fun toString() = symbol
 }
