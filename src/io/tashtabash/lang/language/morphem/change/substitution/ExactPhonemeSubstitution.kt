70cf484ca4fdf8adf37ba10349010a0f562c25fc
smp <matveyshnytkin@gmail.com>
2024-09-02 17:14:10 +0400

    Make ExactPhonemeSubstitution data class

--- src/io/tashtabash/lang/language/morphem/change/substitution/ExactPhonemeSubstitution.kt
+++ src/io/tashtabash/lang/language/morphem/change/substitution/ExactPhonemeSubstitution.kt
@@ -3,7 +3,7 @@ package io.tashtabash.lang.language.morphem.change.substitution
 import io.tashtabash.lang.language.phonology.Phoneme
 
 
-class ExactPhonemeSubstitution(val exactPhoneme: Phoneme) : PhonemeSubstitution {
+data class ExactPhonemeSubstitution(val exactPhoneme: Phoneme) : PhonemeSubstitution {
     override fun substitute(phoneme: Phoneme?): Phoneme = exactPhoneme
 
     override fun getSubstitutePhoneme() = exactPhoneme
