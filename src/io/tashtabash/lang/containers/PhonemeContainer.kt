9f9847dd54014c604481dca8ddfa7c6c8951bec4
smp <matveyshnytkin@gmail.com>
2024-09-05 17:00:25 +0400

    Add new getters to PhonemeContainer

--- src/io/tashtabash/lang/containers/PhonemeContainer.kt
+++ src/io/tashtabash/lang/containers/PhonemeContainer.kt
@@ -3,13 +3,22 @@ package io.tashtabash.lang.containers
 import io.tashtabash.lang.language.phonology.Phoneme
 import io.tashtabash.lang.language.phonology.PhonemeType
 
+
 interface PhonemeContainer {
     val phonemes: List<Phoneme>
 
     val size: Int
         get() = phonemes.size
 
-    fun getPhonemes(phonemeType: PhonemeType): List<Phoneme> = phonemes.filter { it.type == phonemeType }
+    fun getPhoneme(symbol: String): Phoneme =
+        phonemes.first { it.symbol == symbol }
 
-    fun getPhonemesNot(phonemeType: PhonemeType): List<Phoneme> = phonemes.filter { it.type != phonemeType }
+    fun getPhonemeOrNull(symbol: String): Phoneme? =
+        phonemes.firstOrNull { it.symbol == symbol }
+
+    fun getPhonemes(phonemeType: PhonemeType): List<Phoneme> =
+        phonemes.filter { it.type == phonemeType }
+
+    fun getPhonemesNot(phonemeType: PhonemeType): List<Phoneme> =
+        phonemes.filter { it.type != phonemeType }
 }
