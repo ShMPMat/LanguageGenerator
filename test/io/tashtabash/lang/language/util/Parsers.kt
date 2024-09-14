9a388e62642163bf211a27fdd3cab5b8b76b5b14
smp <matveyshnytkin@gmail.com>
2024-09-14 15:26:35 +0400

    Add createTemplateChange(..) test fun

--- test/io/tashtabash/lang/language/util/Parsers.kt
+++ test/io/tashtabash/lang/language/util/Parsers.kt
@@ -1,7 +1,6 @@
 package io.tashtabash.lang.language.util
 
 import io.tashtabash.lang.containers.ImmutablePhonemeContainer
-import io.tashtabash.lang.language.diachronicity.PhonologicalRule
 import io.tashtabash.lang.language.diachronicity.createPhonologicalRule
 import io.tashtabash.lang.language.morphem.Affix
 import io.tashtabash.lang.language.morphem.Prefix
@@ -9,12 +8,9 @@ import io.tashtabash.lang.language.morphem.Suffix
 import io.tashtabash.lang.language.morphem.change.Position
 import io.tashtabash.lang.language.morphem.change.TemplateSequenceChange
 import io.tashtabash.lang.language.morphem.change.TemplateSingleChange
-import io.tashtabash.lang.language.morphem.change.substitution.DeletingPhonemeSubstitution
 import io.tashtabash.lang.language.morphem.change.substitution.ExactPhonemeSubstitution
-import io.tashtabash.lang.language.morphem.change.substitution.PassingPhonemeSubstitution
 import io.tashtabash.lang.language.morphem.change.substitution.createPhonemeSubstitution
 import io.tashtabash.lang.language.phonology.PhonemeType
-import io.tashtabash.lang.language.phonology.matcher.PassingPhonemeMatcher
 import io.tashtabash.lang.language.phonology.matcher.createPhonemeMatcher
 
 
@@ -45,14 +41,23 @@ fun createAffix(vararg affixes: String): Affix {
         Prefix(change)
 }
 
+fun createAffix(affix: String): Affix {
+    val templateChange = createTemplateChange(affix)
+
+    return if (templateChange.position == Position.Beginning)
+        Prefix(templateChange)
+    else
+        Suffix(templateChange)
+}
+
 // Two different cases: with changing the stem and not changing the stem
-fun createAffix(affix: String) = when {
-    affix.contains("->") -> {
-        val (matchers, substitutions) = affix.split("->")
+fun createTemplateChange(templateChange: String): TemplateSingleChange = when {
+    templateChange.contains("->") -> {
+        val (matchers, substitutions) = templateChange.split("->")
             .map { it.trim() }
 
         if (matchers[0] == '-')
-            Suffix(TemplateSingleChange(
+            TemplateSingleChange(
                 Position.End,
                 matchers.drop(1)
                     .map { createTestPhonemeMatcher(it) },
@@ -60,9 +65,9 @@ fun createAffix(affix: String) = when {
                     .map { createTestPhonemeSubstitution(it) },
                 substitutions.drop(matchers.length - 1)
                     .map { createTestPhonemeSubstitution(it) as ExactPhonemeSubstitution },
-            ))
+            )
         else
-            Prefix(TemplateSingleChange(
+            TemplateSingleChange(
                 Position.Beginning,
                 matchers.dropLast(1)
                     .map { createTestPhonemeMatcher(it) },
@@ -70,25 +75,25 @@ fun createAffix(affix: String) = when {
                     .map { createTestPhonemeSubstitution(it) },
                 substitutions.dropLast(matchers.length - 1)
                     .map { createTestPhonemeSubstitution(it) as ExactPhonemeSubstitution },
-            ))
+            )
     }
-    affix[0] == '-' -> {
-        Suffix(TemplateSingleChange(
+    templateChange[0] == '-' -> {
+        TemplateSingleChange(
             Position.End,
             listOf(),
             listOf(),
-            affix.drop(1).map { ExactPhonemeSubstitution(makePhoneme(it)) }
-        ))
+            templateChange.drop(1).map { ExactPhonemeSubstitution(makePhoneme(it)) }
+        )
     }
-    affix.last() == '-' -> {
-        Prefix(TemplateSingleChange(
+    templateChange.last() == '-' -> {
+        TemplateSingleChange(
             Position.Beginning,
             listOf(),
             listOf(),
-            affix.dropLast(1).map { ExactPhonemeSubstitution(makePhoneme(it)) }
-        ))
+            templateChange.dropLast(1).map { ExactPhonemeSubstitution(makePhoneme(it)) }
+        )
     }
-    else -> throw Exception("Incorrect test affix format: '$affix'")
+    else -> throw Exception("Incorrect test affix format: '$templateChange'")
 }
 
 fun createPhonemeContainer(phonemes: String) =
