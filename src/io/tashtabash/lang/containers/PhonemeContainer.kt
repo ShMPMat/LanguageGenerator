e2d386f697255f9ffe4c22a01efaa50301aa318b
smp <matveyshnytkin@gmail.com>
2024-09-17 17:15:46 +0400

    Add funs to PhonemeContainer

--- src/io/tashtabash/lang/containers/PhonemeContainer.kt
+++ src/io/tashtabash/lang/containers/PhonemeContainer.kt
@@ -1,6 +1,8 @@
 package io.tashtabash.lang.containers
 
+import io.tashtabash.lang.language.LanguageException
 import io.tashtabash.lang.language.phonology.Phoneme
+import io.tashtabash.lang.language.phonology.PhonemeModifier
 import io.tashtabash.lang.language.phonology.PhonemeType
 
 
@@ -15,6 +17,29 @@ interface PhonemeContainer {
 
     fun getPhonemeOrNull(symbol: String): Phoneme? =
         phonemes.firstOrNull { it.symbol == symbol }
+
+    fun getPhonemeByPropertiesOrNull(phoneme: Phoneme): Phoneme? =
+        phonemes.firstOrNull { it.isEqualByProperties(phoneme) }
+
+    fun getPhonemeWithAddedModifiers(phoneme: Phoneme, modifiers: Set<PhonemeModifier>): Phoneme {
+        modifiers.firstOrNull { it in phoneme.modifiers }
+            ?.let {
+                throw LanguageException("Can't add modifier '$it' to phoneme '$phoneme' already containing it")
+            }
+
+        val newPhoneme = phoneme.copy(modifiers = phoneme.modifiers + modifiers)
+        val correctNamePhoneme = getPhonemeByPropertiesOrNull(newPhoneme)
+            ?: throw LanguageException(
+                "Phoneme with properties " +
+                        "${newPhoneme.type}, " +
+                        "${newPhoneme.articulationPlace}, " +
+                        "${newPhoneme.articulationManner}, " +
+                        "${newPhoneme.modifiers}, " +
+                        "doesn't exist"
+            )
+
+        return correctNamePhoneme
+    }
 
     fun getPhonemes(phonemeType: PhonemeType): List<Phoneme> =
         phonemes.filter { it.type == phonemeType }
