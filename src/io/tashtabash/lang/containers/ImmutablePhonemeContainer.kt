d503e21100fd266f21d3b1dda073b3ad94226029
smp <matveyshnytkin@gmail.com>
2024-11-01 16:49:17 +0400

    Add order to ImmutablePhonemeContainer.toString()

--- src/io/tashtabash/lang/containers/ImmutablePhonemeContainer.kt
+++ src/io/tashtabash/lang/containers/ImmutablePhonemeContainer.kt
@@ -1,9 +1,19 @@
 package io.tashtabash.lang.containers
 
+import io.tashtabash.lang.language.phonology.ArticulationPlace
 import io.tashtabash.lang.language.phonology.Phoneme
 import io.tashtabash.lang.language.phonology.PhonemeType
+import java.util.Comparator
 
 
 data class ImmutablePhonemeContainer(override val phonemes: List<Phoneme>) : PhonemeContainer {
-    override fun toString() = "$phonemes"
+    override fun toString() = phonemes.groupBy { it.type }
+        .entries
+        .joinToString { (type, phonemes) ->
+            val sortedPhonemes = phonemes
+                .sortedWith(Comparator.comparing<Phoneme?, ArticulationPlace?> { it.articulationPlace }
+                .then(Comparator.comparing { it.articulationManner }))
+
+            "$type: $sortedPhonemes"
+        }
 }
