d2335ed52145cc00a63ce89d786b06d8cd3029fc
smp <matveyshnytkin@gmail.com>
2024-11-03 14:34:59 +0400

    Refactor Connotation to increase performance

--- src/io/tashtabash/lang/language/lexis/Connotation.kt
+++ src/io/tashtabash/lang/language/lexis/Connotation.kt
@@ -4,8 +4,10 @@ import io.tashtabash.lang.language.LanguageException
 
 
 data class Connotation(var name: String, val strength: Double, var isGlobal: Boolean = false) {
-    fun getCompatibility(that: Connotation) = (if (name == that.name) 1.0
-    else connotationsCompatibility[name]?.get(that.name))?.times(strength)
+    fun getCompatibility(that: Connotation): Double? = (
+            if (name == that.name) 1.0
+            else connotationsCompatibility[name]?.get(that.name)
+            )?.times(strength)
 
     operator fun plus(that: Connotation): Connotation {
         if (name != that.name)
@@ -30,57 +32,89 @@ data class Connotation(var name: String, val strength: Double, var isGlobal: Boo
     override fun toString() = "$name:$strength"
 }
 
-data class Connotations(val values: Set<Connotation> = setOf()) {
-    constructor(values: List<Connotation>): this(values.toSet())
+class Connotations internal constructor(val _values: LinkedHashMap<Connotation, Connotation>) {
+    constructor(values: Set<Connotation>) : this(LinkedHashMap<Connotation, Connotation>().apply {
+        values.forEach {
+            set(it, it)
+        }
+    })
+
+    constructor(values: List<Connotation> = listOf()) : this(LinkedHashMap<Connotation, Connotation>().apply {
+        values.forEach {
+            set(it, it)
+        }
+    })
+
+    constructor(vararg connotations: Connotation): this(connotations.asList())
+
+    val values = _values.keys
 
     operator fun plus(that: Connotations): Connotations {
-        val values = values.toMutableSet()
+        val newValues = LinkedHashMap<Connotation, Connotation>()
+        values.forEach { newValues[it] = it }
 
         for (value in that.values) {
-            if (value in values) {
-                val newConnotation = value + values.first { it == value }
-                values.remove(value)
-                values += newConnotation
-            } else
-                values += value
+            newValues[value]?.let {
+                val newConnotation = value + it
+                newValues[value] = newConnotation
+            } ?: run {
+                newValues[value] = value
+            }
         }
 
-        return Connotations(values)
+        return Connotations(newValues)
     }
 
-    override fun toString() = if (values.isNotEmpty())
+    infix fun distance(that: Connotations): Double {
+        val mutualConnotations = values.mapNotNull { c ->
+            val other = that._values[c]
+                ?: return@mapNotNull null
+
+            c to other
+        }
+        val nonMutualElementsNumber = _values.size + that._values.size - 2 * mutualConnotations.size
+
+        if (_values.isEmpty() || that._values.isEmpty())
+            return 0.0
+
+        return mutualConnotations
+            .map { (c1, c2) -> c1.strength * c2.strength }
+            .toMutableList()
+            .apply { addAll(List(nonMutualElementsNumber) { 0.0 }) }
+            .average()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Connotations
+
+        if (values != other.values) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return values.hashCode()
+    }
+
+    override fun toString() = if (_values.isNotEmpty())
         values.joinToString()
     else "no connotations"
 }
 
-infix fun Connotations.distance(that: Connotations): Double {
-    val mutualConnotations = values.mapNotNull { c ->
-        val other = that.values.firstOrNull { c == it }
-            ?: return@mapNotNull null
-
-        c to other
-    }
-    val otherElementsAmount = values.size + that.values.size - 2 * mutualConnotations.size
-
-    if (values.isEmpty() || that.values.isEmpty())
-        return 0.0
-
-    return mutualConnotations
-        .map { (c1, c2) -> c1.strength * c2.strength }
-        .toMutableList()
-        .apply { addAll(List(otherElementsAmount) { 0.0 }) }
-        .average()
-}
 
 infix fun List<Connotation>.distance(that: List<Connotation>) =
     Connotations(this) distance Connotations(that)
 
 infix fun Connotations.localDistance(that: Connotations): Double {
     val globalConnotations = values
-        .filter { it.isGlobal && that.values.firstOrNull { c -> c == it }?.isGlobal ?: true } +
-            that.values.filter { it.isGlobal && values.firstOrNull { c -> c == it }?.isGlobal ?: true }
+        .filter { it.isGlobal && that._values[it]?.isGlobal ?: true } +
+            that.values.filter { it.isGlobal && _values[it]?.isGlobal ?: true }
 
-    return values.filter { it !in globalConnotations } distance that.values.filter { it !in globalConnotations }
+    return values
+        .filter { it !in globalConnotations } distance that.values.filter { it !in globalConnotations }
 }
 
 
