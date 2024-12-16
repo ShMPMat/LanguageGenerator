ecdca4b89ce793bcd71f9a9f891b331596ccdc64
ShMPMat <matveyshnytkin@gmail.com>
2024-12-16 18:58:17 +0400

    Add LatchedWord.map

--- src/io/tashtabash/lang/language/syntax/sequence/LatchedWord.kt
+++ src/io/tashtabash/lang/language/syntax/sequence/LatchedWord.kt
@@ -3,4 +3,7 @@ package io.tashtabash.lang.language.syntax.sequence
 import io.tashtabash.lang.language.lexis.Word
 
 
-data class LatchedWord(val word: Word, val latchType: LatchType)
+data class LatchedWord(val word: Word, val latchType: LatchType) {
+    fun map(mapper: (Word) -> Word): LatchedWord =
+        copy(word = mapper(word))
+}
