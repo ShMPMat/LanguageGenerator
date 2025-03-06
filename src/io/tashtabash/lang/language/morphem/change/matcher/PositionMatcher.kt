c29ef5f0aa914c63da275bcad98cf0dbcd692512
ShMPMat <matveyshnytkin@gmail.com>
2025-03-06 08:47:16 +0400

    Remove PositionMatcher

--- src/io/tashtabash/lang/language/morphem/change/matcher/PositionMatcher.kt
+++
@@ -1,8 +0,0 @@
-package io.tashtabash.lang.language.morphem.change.matcher
-
-import io.tashtabash.lang.language.phonology.Syllables
-
-
-interface PositionMatcher {
-    fun test(syllables: Syllables): Boolean
-}
