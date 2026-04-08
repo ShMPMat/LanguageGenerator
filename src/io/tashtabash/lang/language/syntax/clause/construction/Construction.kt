5bd778fe351c1c1763e48e9ce2e94dc4f2c60923
ShMPMat <matveyshnytkin@gmail.com>
2026-04-08 09:58:04 +0400

    Add Construction comment

--- src/io/tashtabash/lang/language/syntax/clause/construction/Construction.kt
+++ src/io/tashtabash/lang/language/syntax/clause/construction/Construction.kt
@@ -1,4 +1,8 @@
 package io.tashtabash.lang.language.syntax.clause.construction
 
 
+/**
+ * Helpers for translating ClauseDescriptions into other entities in the sentence constructions pipeline,
+ * either other ClauseDescriptions or SyntaxClauses.
+ */
 interface Construction
