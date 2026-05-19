173bdc03399d0685248d7172367c56a00f7cab5b
ShMPMat <matveyshnytkin@gmail.com>
2026-05-19 09:20:26 +0400

    Remove WordSyntaxRole

--- src/io/tashtabash/lang/language/syntax/features/WordSyntaxRole.kt
+++
@@ -1,9 +0,0 @@
-package io.tashtabash.lang.language.syntax.features
-
-
-enum class WordSyntaxRole(val short: String) {
-    Copula("COP"),
-    PersonalPronoun("PP"),
-    Demonstrative("DEM"),
-    QuestionMarker("QM")
-}
