60c8210c9d4989f1fa15e71e1d1bb12d6e45f411
ShMPMat <matveyshnytkin@gmail.com>
2026-08-20 09:24:47 +0400

    Combine Voicing + Devoicing in one file

--- src/io/tashtabash/lang/language/diachronicity/tendency/Voicing.kt
+++ src/io/tashtabash/lang/language/diachronicity/tendency/Voicing.kt
@@ -32,3 +32,26 @@ class Voicing : DefaultPhonologicalChangeTendency() {
             ) + (createWeakRules("(C[-Voiced]) -> [+Voiced] / V _ V") allWithProb 0.1)
         }
 }
+
+
+class Devoicing : DefaultPhonologicalChangeTendency() {
+    override fun getNewInstance() = Devoicing()
+
+    override fun computeDevelopmentChance(language: Language): Double =
+        language.phonemeContainer
+            .getPhonemes(PhonemeModifier.Voiced)
+            .count { it.type == PhonemeType.Consonant }
+            .toDouble()
+
+    override val defaultRetentionChance = 0.8
+
+    override fun getOptions(language: Language, phonemes: PhonemeContainer): List<GenericSSO<PhonologicalRule>> =
+        phonemes.createPhonologicalRulesFor(language) {
+            return listOf(
+                createRule("(C[+Voiced]) -> [-Voiced] / _ $") withProb 0.1,
+                createRule("(C[+Voiced]) -> [-Voiced] / $ _ ") withProb 0.1,
+                createRule("(C[+Voiced]) -> [-Voiced] / [-Voiced] _ ") withProb 0.1,
+                createRule("(C[+Voiced]) -> [-Voiced] /  _ [-Voiced]") withProb 0.1,
+            )
+        }
+}
